6a5eb3c3767481a8aea230525e877a46cc8447ae
marcel <mscherzer@gmail.com>
2022-10-30 12:07:42 +1000

    publish script now executes relative to script loaction. always package from a clean dist folder.

--- publish.sh
+++ publish.sh
@@ -1,9 +1,13 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                     
 # exit when any command fails
 set -e
 
+# Run relative to the location of this script.
+cd "$(dirname "$(readlink -f "$0")")"
+
 # Build and publish this project to NPM.
 yarn test
+rm -Rdf ./dist/*
 yarn build
 chmod +x ./dist/index.js
 yarn publish --public
